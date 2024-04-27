5ce726f14bc80ce7dcb8bf382d19c8fc716466eb
AlexeyQQQ <leadterrier@gmail.com>
2024-04-27 12:21:39 +0300

    LA-04 activity

--- app/src/main/java/com/example/lemonadeapp/MainActivity.kt
+++ app/src/main/java/com/example/lemonadeapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.lemonadeapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lemonadeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel = MainViewModel()
+
+        binding.actionButton.setOnClickListener {
+            val uiState = viewModel.handleAction()     //????????
+            uiState.update(binding)
+        }
+
+        binding.pictureImageButton.setOnClickListener {
+            val uiState = viewModel.clickOnPicture()
+            uiState.update(binding)
+        }
+
+        val uiState = viewModel.init()
+        uiState.update(binding)
     }
 }
