8093fe5167ab7630b43627cd11bc0eea46c27bd2
AlexeyQQQ <leadterrier@gmail.com>
2024-05-26 17:26:45 +0300

    LA-25 Load / scenario test

--- app/src/androidTest/java/com/example/lemonadeapp/ScenarioTest.kt
+++ app/src/androidTest/java/com/example/lemonadeapp/ScenarioTest.kt
@@ -2,6 +2,8 @@ package com.example.lemonadeapp
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.lemonadeapp.game_page.GamePage
+import com.example.lemonadeapp.load_page.LoadPage
 import com.example.lemonadeapp.main.presentation.MainActivity
 import org.junit.Assert.*
 import org.junit.Rule
@@ -17,21 +19,54 @@ class ScenarioTest {
 
     /**
      * TestCase N1: сorrect
-     * 1 Нажать на кнопку 1 - перешли на #2 State: Start squeezing
-     * 2 Тапнуть 5 раз по картинке лимона - перешли на #3 State: Finish squeezing
-     * 3 Нажали на кнопку 1 - перешли на #4 State: Lemonade is ready
-     * 4 Нажали на кнопку 1 - перешли на #5 State: Finish game
-     * 5 Нажали на кнопку 1 - вернулись на #1 State: New game
+     * 1 Нажать на кнопку 1 - перешли на Load (state Progress)
+     *
+     * 1.1 Ожидание ошибки - state Error
+     * 1.2 Клик по Retry button - state Progress
+     * 1.3 Ожидание загрузки - перешли на Start squeezing
+     *
+     * 2 Тапнуть 5 раз по картинке лимона - перешли на Finish squeezing
+     * 3 Нажали на кнопку 1 - перешли на Lemonade is ready
+     * 4 Нажали на кнопку 1 - перешли на Finish game
+     * 5 Нажали на кнопку 1 - вернулись на New game
      */
     @Test
     fun testCase1() {
+
+        // step 0
         val gamePage = GamePage()
         gamePage.checkStateNewGame()
 
         activityScenarioRule.scenario.recreate()
         gamePage.checkStateNewGame()
 
+        // step 1
         gamePage.clickActionButton()
+
+        val loadPage = LoadPage()
+        loadPage.checkProgressState()
+
+        activityScenarioRule.scenario.recreate()
+        loadPage.checkProgressState()
+
+        // step 1.1
+        loadPage.waitError()
+        loadPage.checkErrorState(message = "No internet connection")
+
+        activityScenarioRule.scenario.recreate()
+        loadPage.checkErrorState(message = "No internet connection")
+
+        // step 1.2
+        loadPage.clickRetry()
+        loadPage.checkProgressState()
+
+        activityScenarioRule.scenario.recreate()
+        loadPage.checkProgressState()
+
+        // step 1.3
+        loadPage.waitDisappear()
+
+        // step 2
         repeat(5) {
             gamePage.checkStateStartSqueezing()
             activityScenarioRule.scenario.recreate()
@@ -43,18 +78,21 @@ class ScenarioTest {
         activityScenarioRule.scenario.recreate()
         gamePage.checkStateFinishSqueezing()
 
+        // step 3
         gamePage.clickActionButton()
         gamePage.checkStateLemonadeIsReady()
 
         activityScenarioRule.scenario.recreate()
         gamePage.checkStateLemonadeIsReady()
 
+        // step 4
         gamePage.clickActionButton()
         gamePage.checkStateFinishGame()
 
         activityScenarioRule.scenario.recreate()
         gamePage.checkStateFinishGame()
 
+        // step 5
         gamePage.clickActionButton()
         gamePage.checkStateNewGame()
 
