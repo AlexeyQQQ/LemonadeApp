7a5a7c655f619c1c6296abdd9203ed641c67859e
AlexeyQQQ <leadterrier@gmail.com>
2024-05-27 13:45:45 +0300

    LA-31 Load / UiObservable

--- app/src/main/java/com/example/lemonadeapp/load/presentation/UiObservable.kt
+++ app/src/main/java/com/example/lemonadeapp/load/presentation/UiObservable.kt
@@ -12,4 +12,31 @@ interface UpdateObserver {
     fun clearObserver()
 }
 
-interface UiObservable : UpdateUi, UpdateObserver
+interface UiObservable : UpdateUi, UpdateObserver {
+
+    class Base : UiObservable {
+
+        private var observerShowUi: ((LoadUiState) -> Unit)? = null
+        private var cachedUiState: LoadUiState? = null
+
+        override fun updateUiState(uiState: LoadUiState) {
+            if (observerShowUi != null) {
+                observerShowUi!!.invoke(uiState)
+            } else {
+                cachedUiState = uiState
+            }
+        }
+
+        override fun updateObserver(observer: (LoadUiState) -> Unit) {
+            observerShowUi = observer
+            if (cachedUiState != null) {
+                observer.invoke(cachedUiState!!)
+                cachedUiState = null
+            }
+        }
+
+        override fun clearObserver() {
+            observerShowUi = null
+        }
+    }
+}
