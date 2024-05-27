91f11a41bd6b8c0831c340413e5a236f023b4755
AlexeyQQQ <leadterrier@gmail.com>
2024-05-27 14:09:55 +0300

    LA-32 Load / LoadRepository

--- app/src/main/java/com/example/lemonadeapp/load/data/LoadRepository.kt
+++ app/src/main/java/com/example/lemonadeapp/load/data/LoadRepository.kt
@@ -1,9 +1,33 @@
 package com.example.lemonadeapp.load.data
 
+import com.example.lemonadeapp.core.data.StringCache
+import com.example.lemonadeapp.load.presentation.LoadScreen
+
 interface LoadRepository {
 
     fun load(): LoadResult
 
     fun saveLastScreen()
+
+    class Base(
+        private val lastSavedScreen: StringCache,
+        private val cacheDataSource: CacheDataSource,
+        private val cloudDataSource: CloudDataSource,
+    ) : LoadRepository {
+
+        override fun load(): LoadResult {
+            return try {
+                val data: Int = cloudDataSource.data()
+                cacheDataSource.save(data)
+                LoadResult.Success
+            } catch (e: Exception) {
+                LoadResult.Error(e.message ?: "Unknown error in LoadRepository")
+            }
+        }
+
+        override fun saveLastScreen() {
+            LoadScreen::class.java.canonicalName?.let { lastSavedScreen.save(it) }
+        }
+    }
 
 }
